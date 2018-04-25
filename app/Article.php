2c0add76a2cb714ed7774d360df5fb8064d0f3f4
mingquan.li <mingquan.li@5262.com>
2018-04-25 12:45:46 +0800

    设置可以被批量赋值的字段 不能被批量赋值的字段 生成的填充器 php artisan make:seeder ArticlesTableSeeder

--- app/Article.php
+++ app/Article.php
@@ -6,5 +6,26 @@ use Illuminate\Database\Eloquent\Model;
 
 class Article extends Model
 {
-    //
+    //Article::create 和 Article::update 方法中可以使用它们
+    /**
+     * 可以被批量赋值的字段
+     *
+     * @var array
+     */
+    protected $fillable = ['title', 'bosy'];
+
+
+    /**
+     * 不能被批量赋值的属性
+     *
+     * @var array
+     */
+    protected $guarded = [];
+
+
+
+
+
+
+
 }
